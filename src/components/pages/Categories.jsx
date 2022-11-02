6e283835885ca16b5f303f57e208d34fbeb5e210
StefanDsd <stefan_dascalescu92@gmail.com>
2022-11-02 17:28:20 +0100

    5th

--- src/components/pages/Categories.jsx
+++ src/components/pages/Categories.jsx
@@ -1,12 +1,47 @@
 import React from 'react'
 import CategoriesList from "../categories-list"
 import CategoryItem from "../category-item"
+import axios from "axios"
+
+
+
+const data = [
+    {
+      id: 1,
+      name: "Clothes",
+      image: "https://api.lorem.space/image/fashion?w=640&h=480&r=6011",
+    },
+    {
+      id: 2,
+      name: "Electronics",
+      image: "https://api.lorem.space/image/watch?w=640&h=480&r=304",
+    },
+    {
+      id: 3,
+      name: "Furniture",
+      image: "https://api.lorem.space/image/furniture?w=640&h=480&r=9450",
+    },
+    {
+      id: 4,
+      name: "Shoes",
+      image: "https://api.lorem.space/image/shoes?w=640&h=480&r=3133",
+    },
+    {
+      id: 5,
+      name: "Others",
+      image: "https://api.lorem.space/image?w=640&h=480&r=4432",
+    },
+  ];
 
 const Categories = () => {
+    axios
+    .get(`https://api.escuelajs.co/api/v1/categories`)
+      .then((response) => console.log(response))
+      .catch((error) => console.log(error, "error"));
   return (
     <div>
-       <CategoriesList/>
-       <CategoryItem />
+       <CategoriesList data={data}/>
+       {/* <CategoryItem /> */}
     </div>
   )
 }
